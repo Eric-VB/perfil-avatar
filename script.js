a9d7be3b9617c82bef63906888389ada1a7f503d
Eric-VB <eric.viniciusbasso@gmail.com>
2024-10-21 15:53:46 -0300

    Reagindo a alteração da imagem

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const input = document.querySelector('#inputFile');
 const campoImg = document.querySelector('#img');
-
+let atualizaFoto = 0;
 
 input.addEventListener('change', (event) => {
 
@@ -12,4 +12,19 @@ input.addEventListener('change', (event) => {
 
     campoImg.src = imagem;
 
+    atualizaFoto = 1;
+    
+    setTimeout(() => {
+        dialog()
+    }, 2000)
 })
+
+function dialog() {
+    
+    if(atualizaFoto == 1){
+        atualizaFoto = 0;
+    }
+    
+    console.log(atualizaFoto)
+    
+}
