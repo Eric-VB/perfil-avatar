0708ed6da041c22a1f48029892da9e9d101a01ca
Eric-VB <eric.viniciusbasso@gmail.com>
2024-10-23 23:39:00 -0300

    Finalizando mensagem de imagem pesada e adicionando logica de aparição do aviso

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
 const input = document.querySelector('#inputFile');
+
 const campoImg = document.querySelector('#img');
+
 const elementoDialog = document.querySelector('#dialog');
+
+const tamanhoAtual = document.querySelector('#tamanhoAtual');
+
+const conteudoCard = document.querySelector('#conteudoCard');
+
+const cardPrincipal = document.querySelector('#cardPrincipal');
 
 let atualizaFoto = false;
 
+// ------------------------------------------------------------
 
 input.addEventListener('change', (event) => {
 
-    let fileInput =  event.target.files;
+    let fileInput =  event.target.files[0];
 
-    let inputSelecionado = fileInput[0];
-
-    let imagem = URL.createObjectURL(inputSelecionado);
-
-    campoImg.src = imagem;
-
-    atualizaFoto = true;
+    let fileTamanho = fileInput.size;
+    let fileTamanhoKB = (fileTamanho / 1024).toFixed(2);
     
-    setTimeout(() => {
-        openDialog()
-    }, 1500);
+    if(fileTamanhoKB > 15){
+
+        cardPrincipal.style.height = '70%';
+
+        conteudoCard.style.display = 'flex';
+
+        tamanhoAtual.textContent = `Tamanho do arquivo selecionado ${fileTamanhoKB} KB`;
+    
+    } else {
+
+        let imagem = URL.createObjectURL(fileInput);
+    
+        campoImg.src = imagem;
+    
+        atualizaFoto = true;
+        
+        setTimeout(() => {
+            openDialog()
+        }, 1500);
+
+        cardPrincipal.style.height = 'auto';
+
+        conteudoCard.style.display = 'none';
+
+        console.log(fileTamanhoKB)
+    }
 })
 
 function openDialog() {
